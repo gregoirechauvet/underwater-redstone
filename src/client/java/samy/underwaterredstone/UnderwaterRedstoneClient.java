e631c0c47d2f3e086753fcc27eaf24e09dfdfbce
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2025-11-02 10:30:51 +0100

    Project setup

---
+++ src/client/java/samy/underwaterredstone/UnderwaterRedstoneClient.java
@@ -0,0 +1,10 @@
+package samy.underwaterredstone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnderwaterRedstoneClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
